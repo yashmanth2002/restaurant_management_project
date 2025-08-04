81c670604db644fcf80b81bd6362b91eddd9acd7
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-04 13:53:13 +0530

    19286

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class MenuAPIView(APIView):
+    def get(self, request):
+        menu = [
+            {"name": "Spaghetti Carbonara", "description": "Classic Italian pasta with creamy sauce and pancetta.", "price": 12.99},
+            {"name": "Margherita Pizza", "description": "Traditional pizza with tomato, mozzarella, and basil.", "price": 10.99},
+            {"name": "Caesar Salad", "description": "Romaine letuce with Caesar dressing, croutons, and parmesan.", "price": 8.99},
+        ]
+
+        return Response(menu, status=status.HTTP_200_OK)
