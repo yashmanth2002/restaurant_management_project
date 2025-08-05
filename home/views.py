3e8fb7a6cb56ee5f426d713e12f1e7138ecec239
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-05 12:23:58 +0530

    19660

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 # Create your views here.
+def contact_us_view(request):
+    form = ContactForm(request.POST or None)
+    if request.method =='POST' and form.is_valid():
+        form.save()
+        return redirect('home')
+    
+    return render(request, 'contact_us.html', {'form': form})
