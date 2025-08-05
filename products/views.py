173aef440a72e7096e55b23df04b09c62368bec6
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-05 14:21:24 +0530

    22057

--- products/views.py
+++ products/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-
 from .models import Item
 from .serializers import ItemSerializer
+from django.shortcuts import render
+from .models import Restaurant
 
 '''
 NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
@@ -25,3 +25,9 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        def restaurant_page(request):
+            restaurant = Restaurant.objects.first()
+            return render(request, 'restaurant.html', {
+                'restaurant': restaurant
+            })
