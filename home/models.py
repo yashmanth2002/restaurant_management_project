79c42714a45277d5444520123436bf1be65b827d
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-05 12:52:05 +0530

    19662

--- home/models.py
+++ home/models.py
@@ -1,9 +1,17 @@
 from django.db import models
-
+from django.db import models
 # Create your models here.
 class Contact(models.Model):
     name = models.Charfield(max_length=100)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name = models.Charfield(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
