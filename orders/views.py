15d8e63cc32610142444d29f0c5503d3bbd25070
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-05 14:53:41 +0530

    22184

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import render
 
 class MenuAPIView(APIView):
     def get(self, request):
@@ -11,3 +12,13 @@ class MenuAPIView(APIView):
         ]
 
         return Response(menu, status=status.HTTP_200_OK)
+
+    def menu_preview(request):
+
+        menu_items = [
+            {'name': 'Paneer Tikka', 'price': 240},
+            {'name': 'Chicken Biryani', 'price': 320},
+            {'name': 'Masala Dosa', 'price': 180},
+            {'name': 'Gulab Jamun', 'price': 90},
+        ]
+        return render(request, 'menu_preview.html', {'menu_items': menu_items})
