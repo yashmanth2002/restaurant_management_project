e4eccd20470420e1d69554aa4a722b18872ff5ca
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-05 12:08:19 +0530

    19660

--- orders/models.py
+++ orders/models.py
@@ -4,14 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
 from .models import menu, Order
-from django.db import models
 
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.name
 
 admin.site.register(Menu)
 admin.site.register(Order)
