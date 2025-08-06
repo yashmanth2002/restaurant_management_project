db98aef891aa357fb57daa25d074c19af5097c6e
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-06 20:50:08 +0530

    24084

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -6,3 +6,13 @@ class Restaurant(models.model):
 
     def __str__(self):
         return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    image = models.ImageField(upload_to= 'menu_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
