9ba42263f9050ce4a63eeb21a45e35bf471decb6
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-07 20:46:54 +0530

    25773

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('reservations/', reservations_view, name='reservations')
     path('', home, name='home'),
+    path('', home, name='home')
 ]
