229a2bbfd27c0015e393b6f4aaf022225e7a814b
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-07 20:16:22 +0530

    25771

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
@@ -8,3 +9,12 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
