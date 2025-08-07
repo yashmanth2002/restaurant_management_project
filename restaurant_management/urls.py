c246146bfeed93d900f9c9ee23e24fc387301010
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-07 20:08:45 +0530

    25770

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -22,6 +22,8 @@ from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import contact_view, success_view
+from .views import contact_view
+from django.urls import include,path
 
 
 urlpatterns = [
@@ -36,6 +38,8 @@ urlpatterns = [
     path('', include('home.urls')),
     path('contact/', contact_view, name='contact')
     path('success/', success_view, name='success')
+    path('contact/', contact_view, name='contact'),
+    path('', include('contact.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
