57de53f1f781916511e15849b1c2485475a87ce4
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-07 18:53:01 +0530

    24085

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import MenuAPIView
 from .views import menu_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('preview-menu/', menu_preview, name='menu-provider'),
     path('menu/', menu_view, name='menu'),
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 ]
