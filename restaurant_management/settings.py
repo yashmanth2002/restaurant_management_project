f5df1760879591b981f7cf608cb57ddca7cc9b4c
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-07 20:23:58 +0530

    25771

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,3 +151,7 @@ STATICFILES_DIRS = [BASE_DIR/ "static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
