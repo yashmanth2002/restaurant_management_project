250d3a2f67d9862ce9982effe88c22370881d150
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-07 20:38:48 +0530

    25773

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
 from .views import reservations_view
+from .views import home
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('reservations/', reservations_view, name='reservations')
+    path('', home, name='home'),
 ]
