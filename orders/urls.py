f375fa5d088fed3ae9aa25accf4583c13927b150
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-07 18:49:40 +0530

    24085

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import MenuAPIView
+from .views import menu_view
 
 urlpatterns = [
     path('preview-menu/', menu_preview, name='menu-provider'),
+    path('menu/', menu_view, name='menu'),
 ]
