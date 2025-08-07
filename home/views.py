530bcb11c73d738f0e5ef7531c3f7aa3098612ba
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-07 19:29:42 +0530

    25769

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.shortcuts import render
+from .models import MenuItem
 # Create your views here.
 def contact_us_view(request):
     form = ContactForm(request.POST or None)
@@ -12,3 +13,12 @@ def contact_us_view(request):
 
 def contact_page(request):
     return render(request, 'contact.html')
+
+def home(request):
+    query = requests.GET.get('q')
+    if query:
+        menu_items = MenuItem.objects.filter(name___icontains=query)
+
+    else:
+        menu_items = MenuItem.objects.all()
+    return render(request, 'home.html', {'menu_items': menu_items })
