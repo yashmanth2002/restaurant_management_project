a107e85c358309217987ab5dcfa816388c571019
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-07 20:44:24 +0530

    25773

--- products/models.py
+++ products/models.py
@@ -26,3 +26,11 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
