ded95964e8d38cd4248aef18deae249009d0967b
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-08 16:58:57 +0530

    25931

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from .views import menu_view
 from .views import home
 from . import views
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('contact/', contact_page, name='contact'),
     path('', home, name='home'),
     path('about/', views.about_page, name='about')
-]
+    path('contact/', contact_page, name='contact'),
+    path('', home, name='home'),
+    path('about', views.about_page, name='about'),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
