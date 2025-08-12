6e2ff065720d2a3e286b8ab1c67611b24e678873
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-12 12:11:36 +0530

    31004

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,8 @@ from .views import home
 from . import views
 from django.conf.urls.static import static 
 from .views import submit_feedback
+from django.urls import path
+from .views import contact_view, thank_you_view
 
 urlpatterns = [
     path('contact/', contact_page, name='contact'),
@@ -13,5 +15,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('about', views.about_page, name='about'),
     path('feedback/', submit_feedback, name='submit_feedback')
+    path('contact/', contact_view, name='contact')
+    path('contact/thank-you/', thank_you_view, name='thank_you')
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
