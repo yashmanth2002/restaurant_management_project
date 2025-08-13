ef2bdfa2e7e8a03756b0da660b4823401ec9f861
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-13 12:57:18 +0530

    31764

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -194,3 +194,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'youremail@example.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
