b71d8a66b34a40de80caa1e3a39a3204a73b7fc0
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-14 13:30:43 +0530

    32711

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import RestaurantInfo
+from django.contrib import admin
+from .models import RestaurantInfo
 
 # Register your models here.\\
 
+admin.site.register(RestaurantInfo)
 admin.site.register(RestaurantInfo)
