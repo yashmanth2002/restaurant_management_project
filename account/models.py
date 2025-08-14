521b4e08948650d185ff37fe77282416f7c47638
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-14 13:37:38 +0530

    32707

--- account/models.py
+++ account/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.db import models
 
 # Create your models here.
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
+
+class RestaurantInfo(models.Model):
+    name = models.CharField(max_length=200, default="Our Restaurant")
+    address = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
