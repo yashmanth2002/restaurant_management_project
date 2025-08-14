2323a2e02c8bbc41afccde022ae204cbe0c1923f
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-14 13:42:09 +0530

    32707

--- account/views.py
+++ account/views.py
@@ -3,6 +3,9 @@ from .models import Restaurant
 from django.db import models
 from django.shortcuts import render
 from .models import RestaurantInfo
+from django.shortcuts import render
+from .models import RestaurantInfo
+
 # Create your views here.
 
 def homepage(request):
@@ -24,3 +27,7 @@ class Restaurant(models.Model):
 def about_us_view(request):
     info = RestaurantInfo.objects.first()
     return render(request, 'about_us.html', {'info': info})
+
+def contact_us(request):
+    restaurant_info = RestaurantInfo.objects.first()
+    return render(request, 'home/contact.html', {'restaurant_info': restaurant_info})
