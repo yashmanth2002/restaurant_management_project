442b0d7ead46684dbfff2ae1948d819d594e09fa
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-14 16:13:06 +0530

    32757

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -217,3 +217,11 @@ TEMPLATES = [{
         ]
     }
 }]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend',
+EMAIL_HOST = 'smtp.gmail.com',
+EMAIL_PORT = 587,
+EMAIL_USE_TLS = True,
+EMAIL_HOST_USER = 'your_email@example.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+DEFAULT_FROM_EMAIL = 'Your Restaurant <your_email@example.com>'
