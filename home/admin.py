b7d0a15a933b66b0a053dcbc6f8b7a8b4814a1fe
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-20 13:56:44 +0530

    36426

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from . models import RestaurantContact
 # Register your models here.
+from django.contrib import admin
+from .models import ContactInfo
 
 class RestaurantContactAdmin(admin.ModelAdmin):
     list_display = ("address", "phone_number", "email")
+    list_display = ("phone", "email", "address")
