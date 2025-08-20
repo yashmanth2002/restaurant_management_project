7bf28eab7a8541b907cec17f58678b09ace810ed
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-20 13:54:57 +0530

    36426

--- home/models.py
+++ home/models.py
@@ -15,6 +15,7 @@ from django.db import models
 from django.db import models
 from django.db import models
 from django.db import models
+from django.db import models
 
 
 class RestaurantInfo(models.Model):
@@ -206,3 +207,11 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+
+class ContactInfo(models.Model):
+    address = models.CharField(max_length=255, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(blank=True)
+
+    def __str__(self):
+        return f"Contact Info ({self.phone})
