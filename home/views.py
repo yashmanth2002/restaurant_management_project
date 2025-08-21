51fec6f47f432b128814434fdd97198e6beb5588
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-21 14:36:09 +0530

    37015

--- home/views.py
+++ home/views.py
@@ -67,6 +67,10 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.shortcuts import render
+
+def privacy_policy(request):
+    return render(request, "privacy_policy.html")
 
 def contact_us(request):
     if request.method =="POST":
