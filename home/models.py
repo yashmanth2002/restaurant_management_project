2f4c3928360c23cada5e1d9119fb55daae6aadec
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-21 12:06:04 +0530

    36877

--- home/models.py
+++ home/models.py
@@ -17,7 +17,14 @@ from django.db import models
 from django.db import models
 from django.db import models
 from django.db import models
+from django.db import models
 
+class NewsletterSubscription(models.Model):
+    email = models.EmailField(unique=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
 
 class RestaurantInfo(models.Model):
     name = models.CharField(max_length = 200, default="Our Restaurant")
