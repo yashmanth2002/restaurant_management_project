b020b33cdebbf093bc217a2645deb14af0a73f09
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-25 11:24:43 +0530

    37659

--- home/views.py
+++ home/views.py
@@ -73,6 +73,10 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.shortcuts import render
+
+def custom_403(request, exception=None):
+    return render(request, "403.html", status=403)
 
 
 def contact_view(request):
