b88d6451bdde0168ca1a8740c9bbeab6aa071614
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-25 11:56:36 +0530

    38900

--- home/models.py
+++ home/models.py
@@ -18,6 +18,19 @@ from django.db import models
 from django.db import models
 from django.db import models
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+
+class RestaurantReview(models.Model):
+    RATING_CHOICES = [(i, str(i)) for i in range(3,6)]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "reviews")
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.rating}"
 
 class NewsItem:
     def __init___(self, title, content, date):
