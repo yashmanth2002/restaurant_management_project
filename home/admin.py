9336dcb5424f91b0aec77bf196fe4620d1cb3e60
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-25 11:59:24 +0530

    38900

--- home/admin.py
+++ home/admin.py
@@ -3,7 +3,12 @@ from . models import RestaurantContact
 # Register your models here.
 from django.contrib import admin
 from .models import ContactInfo
+from django.contrib import admin
+from . models import RestaurantReview
 
 class RestaurantContactAdmin(admin.ModelAdmin):
     list_display = ("address", "phone_number", "email")
     list_display = ("phone", "email", "address")
+    list_display = ("user", "rating", "created_at")
+    search_fields = ("user_username", "comment")
+    list_filter = ("rating", "craeted_at")
