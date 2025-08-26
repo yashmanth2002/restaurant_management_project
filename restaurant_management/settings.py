9ecb4b2fb6308e37c3c6b94d3f3c284489505020
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-26 13:11:11 +0530

    38962

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -245,3 +245,11 @@ ALLOWED_HOSTS = ['*']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "yourrestaurant@gmail.com"
+EMAIL_HOST_PASSWORD = "your_app_password"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
