aed943d9149f5913b2088b0758c16cb90f9662c2
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-26 14:59:13 +0530

    39425

--- home/models.py
+++ home/models.py
@@ -24,6 +24,16 @@ from django.db import models
 from django.db import models
 from django.shortcuts import render
 from .models import HomePage
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+    cuisine_type = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 def home_view(request):
     homepage = HomePage.objects.first()
