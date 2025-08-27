9d8a5aed59d23c62af62d1d4a8d75a0e94bd6eb6
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-27 16:13:45 +0530

    39748

--- home/models.py
+++ home/models.py
@@ -26,6 +26,18 @@ from django.shortcuts import render
 from .models import HomePage
 from django.db import models
 from django.db import models
+from django.db import models
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    date = models.DateTimeField()
+    time = models.TimeField()
+    guests = models.PositiveIntegerField()
+
+    def __str__(self):
+            return f"Reservation for {self.name} on {self.date} at {self.time}"
 
 
 class MenuItem(models.Model):
