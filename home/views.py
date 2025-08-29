2f79d8252cfb4eeb95ec32f9ca69916fda422542
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-29 12:56:21 +0530

    40443

--- home/views.py
+++ home/views.py
@@ -120,6 +120,10 @@ from django.shortcuts import render
 from .models import Restaurant
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render
+
+def terms_of_service(request):
+    return render(request, 'home/terms_of_service.html')
 
 def privacy_policy(request):
     return render(request, "home/privacy_policy.html", {"now": timezone,now()})
