a779795a2af99a0d1457ef0ffd36a7ee88d84e57
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-29 12:19:15 +0530

    40048

--- home/models.py
+++ home/models.py
@@ -29,6 +29,14 @@ from django.db import models
 from django.db import models
 from django.db import models
 from django.db import models
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    address = models. TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Feedback(models.Model):
