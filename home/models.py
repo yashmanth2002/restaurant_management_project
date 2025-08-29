925207715b8c22b1cae1505325dd33576f43ec00
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-08-29 11:31:04 +0530

    40051

--- home/models.py
+++ home/models.py
@@ -30,6 +30,13 @@ from django.db import models
 from django.db import models
 
 
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 class RestaurantInfo(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
