0da6dca232a1c3b6c6f40681695dc2d5f4fec014
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-09-01 13:25:42 +0530

    41411

--- home/admin.py
+++ home/admin.py
@@ -11,6 +11,14 @@ from django.contrib import admin
 from .models import TermsOfService
 from django.contrib import admin
 from .models import FAQ
+from django.contrib import admin
+from .models import RestaurantInfo
+
+
+@admin.register(RestaurantInfo)
+class RestaurantInfoAdmin(admin.ModelAdmin):
+    list_display = ("name", "phone_number")
+
 
 @admin.register(FAQ)
 class FAQ@Admin(admin.ModelAdmin):
