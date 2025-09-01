7499625b91d9a704e919686b2a812829b3084fc0
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-09-01 13:20:21 +0530

    41412

--- home/models.py
+++ home/models.py
@@ -32,6 +32,14 @@ from django.db import models
 from django.db import models
 from django.db import models
 from django.db import models
+from django.db import models
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+
+    def __str__(self):
+        return self.question
 
 class FAQ(models.Model):
     question = models.CharField(max_length=255)
