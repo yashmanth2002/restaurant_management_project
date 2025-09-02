c80f02593d40cdcd97cfca6d2052c7e5e90ac383
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-09-02 11:40:15 +0530

    41420

--- home/models.py
+++ home/models.py
@@ -37,6 +37,14 @@ from django.db import models
 from django.db import models
 from django.db import models
 from django.db import models
+from django.db import models
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 class special(models.Model):
     title = models.CharField(max_length=100)
