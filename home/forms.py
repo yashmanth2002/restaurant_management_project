2507f769f77966bdb8f5dc1d7f33906d9ceed21f
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-09-02 11:30:14 +0530

    41421

--- home/forms.py
+++ home/forms.py
@@ -7,7 +7,24 @@ from django import forms
 from .models import RestaurantReview
 from django import forms
 from .models import Reservation
+from django import forms
 
+class ContactForm(forms.Form):
+    name = forms.CharField(  
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Your Name'})
+    )
+
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Your Email'})
+    )
+
+    message = forms.CharField(
+        required=True,
+        widget=forms.Textarea(attrs={'placeholder': 'Your Message'})
+    )
 
 class ReservationForm(forms.ModelForm):
     class Meta:
