c1d93e99e6057d2ec73b4222d6169f7507279393
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-09-02 12:05:55 +0530

    41776

--- home/models.py
+++ home/models.py
@@ -38,6 +38,15 @@ from django.db import models
 from django.db import models
 from django.db import models
 from django.db import models
+from django.db import models
+
+class Restaurant(models.Model):
+    name =  models.CharField(max_length=200)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100)
