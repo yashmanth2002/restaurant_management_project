342bc29e2bc9d224ef6ee26d9794cc912e3327b3
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-09-02 12:26:58 +0530

    41772

--- home/views.py
+++ home/views.py
@@ -137,6 +137,16 @@ from .models import MenuCategory
 from django .shortcuts import render, redirect
 from .forms import ContactForm
 from .models import Restaurant
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email=models.EmailField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 def contact_view(request):
     restaurant = Restaurant.objects.first()
