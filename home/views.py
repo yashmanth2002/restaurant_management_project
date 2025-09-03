be473bbab172c2a6cb382878aa0628ef56c74de0
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-09-03 18:31:30 +0530

    41826

--- home/views.py
+++ home/views.py
@@ -139,6 +139,8 @@ from .forms import ContactForm
 from .models import Restaurant
 from django.db import models
 
+
+
 class Restaurant(models.Model):
     name = models.CharField(max_length=200)
     phone = models.CharField(max_length=20, blank=True, null=True)
