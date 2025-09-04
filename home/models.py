21d82f01ac6b50934bd253063ee6e987454ab3db
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-09-04 11:40:15 +0530

    42712

--- home/models.py
+++ home/models.py
@@ -40,7 +40,16 @@ from django.db import models
 from django.db import models
 from django.db import models
 from django.db import models
+from django.db import models
 
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=200)
