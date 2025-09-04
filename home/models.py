4e0089ac7f902c616529dce2688bb5deeed320c6
yashmanth2002 <83199130+yashmanth2002@users.noreply.github.com>
2025-09-04 12:36:16 +0530

    42713

--- home/models.py
+++ home/models.py
@@ -41,6 +41,15 @@ from django.db import models
 from django.db import models
 from django.db import models
 from django.db import models
+from django.db import models
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.price}
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=200)
